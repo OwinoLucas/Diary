0ef050f6a9806c3da8ec4e63c456f975c56208df
Lucas Owino <lucowish35@gmail.com>
2020-03-27 12:08:14 +0300

    Add back ground img

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
 import { DiaryEntryComponent } from './diary-entry/diary-entry.component';
 import { DaysAgoPipe } from './days-ago.pipe';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiaryComponent,
     DiaryEntryComponent,
-    DaysAgoPipe
+    DaysAgoPipe,
+    EntryFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
